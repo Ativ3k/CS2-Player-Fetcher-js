50b6628853d53110ca6f1a6e13a6a83f18005ca6
Dominik Niedziółka <dominik140401@gmail.com>
2024-05-12 08:55:51 +0200

    added: node-cache, party check, local player
    Storing matches and profiles with node-cache to prevent spamming the same profiles/matches from Leetify API.
    
    Party check based on Leetify teammates.
    
    Local player by id from config file.

--- index.js
+++ index.js
@@ -3,25 +3,30 @@ import { cwd } from "process";
 import chalk from "chalk";
 import { table } from "table";
 import fs from "fs";
+import NodeCache from "node-cache";
+import logUpdate from "log-update";
 
 const c = chalk;
-const cfg = JSON.parse(fs.readFileSync("./config.json", "utf8"));
 
 class CS2_Players_Data {
   constructor() {
+    this.debug = false;
+    this.cfg = JSON.parse(fs.readFileSync("./config.json", "utf8"));
     this.steamData = [];
-    this.refreshInterval = cfg.refreshinterval;
+    this.refreshInterval = this.cfg.refreshinterval;
     this.apiLeetify = {
-      range: cfg.maxMatchesLeetify,
+      range: this.cfg.maxMatchesLeetify,
       profile: "https://api.leetify.com/api/profile/",
       games: "https://api.leetify.com/api/games/",
     };
     this.leetifyData = [];
-  }
-
-  table() {
-    const data = [
+    this.cache = new NodeCache({ stdTTL: 3600, checkperiod: 120 });
+    this.dataError = false;
+    this.tableErrorRefresh = false;
+    this.tableData = [
       [
+        c.yellow("Status"),
+        c.yellow("Party"),
         c.yellow("Name"),
         c.yellow("Leetify"),
         c.yellow("Premier"),
@@ -35,9 +40,22 @@ class CS2_Players_Data {
         c.yellow("KDR min/avg/max"),
         c.yellow("Hs% min/avg/max"),
         c.yellow("ReactionTime min/avg/max"),
-        c.yellow("DPR min/avg/max"),
+        c.yellow("ADR min/avg/max"),
       ],
+      ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"],
+      ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"],
+      ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"],
+      ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"],
+      ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"],
+      ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"],
+      ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"],
+      ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"],
+      ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"],
+      ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"],
     ];
+  }
+
+  table() {
     const config = {
       columns: [
         { alignment: "center" },
@@ -54,95 +72,202 @@ class CS2_Players_Data {
         { alignment: "center" },
         { alignment: "center" },
         { alignment: "center" },
+        { alignment: "center" },
+        { alignment: "center" },
       ],
     };
-    this.leetifyData.forEach((player) => {
-      let wins = 0;
-      let tie = 0;
-      player.games.forEach((game) => {
-        if (game.matchResult === "win") {
-          wins++;
-        } else if (game.matchResult === "tie") {
-          tie++;
+
+    const teams = [
+      {
+        icon: "🟠",
+        players: new Set(),
+      },
+      {
+        icon: "🟣",
+        players: new Set(),
+      },
+      {
+        icon: "🟢",
+        players: new Set(),
+      },
+      {
+        icon: "🔵",
+        players: new Set(),
+      },
+    ];
+
+    const teamsHasPlayer = (playerName) => {
+      return teams.some((team) => team.players.has(playerName));
+    };
+
+    const getTeamIcon = (playerName) => {
+      return teams.find((team) => team.players.has(playerName))?.icon || "";
+    };
+
+    // sort by premier rating
+    this.leetifyData.sort((a, b) => {
+      const _a = a.games?.find((game) => game.rankType === 11)?.skillLevel || 0;
+      const _b = b.games?.find((game) => game.rankType === 11)?.skillLevel || 0;
+      return _b - _a;
+    });
+
+    this.leetifyData.forEach((player, i) => {
+      // party check based on leetify teammates
+      const friendsInMatch = player.teammates?.filter((friend) => this.steamData.map((d) => d.steam64Id).includes(friend.steam64Id));
+
+      if (friendsInMatch) {
+        for (const team of teams) {
+          if (team.players.size === 0) {
+            if (!teamsHasPlayer(player.meta.name)) {
+              team.players.add(player.meta.name);
+            }
+            friendsInMatch.forEach((friend) => {
+              if (!teamsHasPlayer(friend.steamNickname)) {
+                team.players.add(friend.steamNickname);
+              }
+            });
+          }
         }
-      });
 
-      let leetify = player.recentGameRatings.leetify * 100;
-      leetify > 2 ? (leetify = c.green(`${leetify.toFixed(2)}`)) : (leetify = c.red(`${leetify.toFixed(2)}`));
+        // clear teams with only one player
+        teams.forEach((team) => {
+          if (team.players.size === 1) {
+            team.players.clear();
+          }
+        });
+      }
 
-      let premier = Number(player.games.find((game) => game.rankType === 11)?.skillLevel) || 0;
-      if (premier !== 0) {
-        if (premier > 29999) {
-          premier = c.hex(`#f1ae35`)(`${premier}`);
-        } else if (premier > 24999) {
-          premier = c.hex(`#eb4c4a`)(`${premier}`);
-        } else if (premier > 19999) {
-          premier = c.hex(`#b12fc1`)(`${premier}`);
-        } else if (premier > 14999) {
-          premier = c.hex(`#8845ff`)(`${premier}`);
-        } else if (premier > 9999) {
-          premier = c.hex(`#4b69fe`)(`${premier}`);
-        } else if (premier > 4999) {
-          premier = c.hex(`#5e98d9`)(`${premier}`);
-        } else {
-          premier = c.hex(`#b0c4d8`)(`${premier}`);
+      let wins = null;
+      let tie = null;
+      let premier = 0;
+      let winRatio = null;
+      const kills = [];
+      if (player.games) {
+        player.games.forEach((game) => {
+          if (game.kills >= 0) {
+            kills.push(game.kills);
+          }
+          if (game.matchResult === "win") {
+            wins++;
+          } else if (game.matchResult === "tie") {
+            tie++;
+          }
+        });
+
+        winRatio = ((wins / (player.games.length - tie)) * 100).toFixed(0) || null;
+        winRatio > 50 ? (winRatio = c.green(`${winRatio}%`)) : (winRatio = c.red(`${winRatio}%`));
+
+        premier = Number(player.games.find((game) => game.rankType === 11)?.skillLevel) || 0;
+        if (premier !== 0) {
+          if (premier > 29999) {
+            premier = c.hex(`#f1ae35`)(`${premier}`);
+          } else if (premier > 24999) {
+            premier = c.hex(`#eb4c4a`)(`${premier}`);
+          } else if (premier > 19999) {
+            premier = c.hex(`#b12fc1`)(`${premier}`);
+          } else if (premier > 14999) {
+            premier = c.hex(`#8845ff`)(`${premier}`);
+          } else if (premier > 9999) {
+            premier = c.hex(`#4b69fe`)(`${premier}`);
+          } else if (premier > 4999) {
+            premier = c.hex(`#5e98d9`)(`${premier}`);
+          } else {
+            premier = c.hex(`#b0c4d8`)(`${premier}`);
+          }
         }
       }
 
-      let aim = player.recentGameRatings.aim;
-      aim > 60 ? (aim = c.green(`${aim.toFixed(2)}`)) : (aim = aim.toFixed(2));
+      let aim = undefined;
+      let pos = undefined;
+      let util = undefined;
+      let leetify = undefined;
 
-      let pos = player.recentGameRatings.positioning;
-      pos > 60 ? (pos = c.green(`${pos.toFixed(2)}`)) : (pos = pos.toFixed(2));
+      if (player.recentGameRatings) {
+        aim = player.recentGameRatings.aim;
+        if (aim > 60) {
+          aim = c.green(`${aim.toFixed(2)}`);
+        } else {
+          aim = c.white(`${aim.toFixed(2)}`);
+        }
 
-      let util = player.recentGameRatings.utility;
-      util > 60 ? (util = c.green(`${util.toFixed(2)}`)) : (util = util.toFixed(2));
+        pos = player.recentGameRatings.positioning;
+        if (pos > 60) {
+          pos = c.green(`${pos.toFixed(2)}`);
+        } else {
+          pos = c.white(`${pos.toFixed(2)}`);
+        }
 
-      let winRatio = (wins / (player.games.length - tie)) * 100;
-      winRatio > 50 ? (winRatio = c.green(`${winRatio.toFixed(0)}%`)) : (winRatio = c.red(`${winRatio.toFixed(0)}%`));
+        util = player.recentGameRatings.utility;
+        if (util > 60) {
+          util = c.green(`${util.toFixed(2)}`);
+        } else {
+          util = c.white(`${util.toFixed(2)}`);
+        }
 
-      let kills = [];
-      let reactionTime = [];
-      let adr = [];
-      let kdr = [];
-      let hsp = [];
-      player.games_full.forEach((game) => {
-        const playerStats = game?.playerStats.find((p) => p.steam64Id === player.meta.steam64Id);
+        leetify = player.recentGameRatings.leetify * 100 || null;
+        leetify > 2 ? (leetify = c.green(`${leetify.toFixed(2)}`)) : (leetify = c.red(`${leetify.toFixed(2)}`));
+      }
+
+      const reactionTime = [];
+      const adr = [];
+      const kdr = [];
+      const hsp = [];
+
+      player.games_full?.forEach((game) => {
+        if (!game) return;
+        const playerStats = game.playerStats.find((p) => p.steam64Id === player.meta.steam64Id);
         if (playerStats) {
-          reactionTime.push(playerStats.reactionTime);
-          adr.push(playerStats.dpr);
-          kdr.push(playerStats.kdRatio);
-          hsp.push(playerStats.hsp * 100);
+          if (playerStats.reactionTime > 0) {
+            reactionTime.push(playerStats.reactionTime * 1000);
+          }
+          if (playerStats.dpr !== undefined) adr.push(playerStats.dpr);
+          if (playerStats.kdRatio !== undefined) kdr.push(playerStats.kdRatio);
+          if (playerStats.kdRatio !== undefined) hsp.push(playerStats.hsp * 100);
         }
       });
-      player.games.forEach((game) => {
-        if (game.kills >= 0) {
-          kills.push(game.kills);
+
+      const minKills = Math.min(...kills).toFixed(0) || null;
+      const avgKills = (kills.reduce((a, b) => a + b, 0) / kills.length).toFixed(0) || null;
+      const maxKills = Math.max(...kills).toFixed(0) || null;
+
+      const minRT = Math.min(...reactionTime).toFixed(0) || null;
+      const avgRT = (reactionTime.reduce((a, b) => a + b, 0) / reactionTime.length).toFixed(0) || null;
+      const maxRT = Math.max(...reactionTime).toFixed(0) || null;
+
+      const minADR = Math.min(...adr).toFixed(2) || null;
+      const avgADR = (adr.reduce((a, b) => a + b, 0) / adr.length).toFixed(2) || null;
+      const maxADR = Math.max(...adr).toFixed(2) || null;
+
+      const minKDR = Math.min(...kdr).toFixed(2) || null;
+      const avgKDR = (kdr.reduce((a, b) => a + b, 0) / kdr.length).toFixed(2) || null;
+      const maxKDR = Math.max(...kdr).toFixed(2) || null;
+
+      const minHSP = Math.min(...hsp).toFixed(0) || null;
+      const avgHSP = (hsp.reduce((a, b) => a + b, 0) / hsp.length).toFixed(0) || null;
+      const maxHSP = Math.max(...hsp).toFixed(0) || null;
+
+      const name = player.meta.name?.length === 0 ? player.meta.steam64Id : player.meta.name;
+
+      const killsStr = `${minKills.padStart(2, " ")}` + ` ${c.yellow("/")} ${avgKills.padStart(2, " ")} ${c.yellow("/")} ${maxKills.padStart(2, " ")}`;
+      const kdrStr = `${minKDR.padStart(4, " ")} ${c.yellow("/")} ${avgKDR.padStart(4, " ")} ${c.yellow("/")} ${maxKDR.padStart(4, " ")}`;
+      const hspStr = `${minHSP.padStart(3, " ")} ${c.yellow("/")} ${avgHSP.padStart(3, " ")} ${c.yellow("/")} ${maxHSP.padStart(3, " ")}`;
+      const rtStr = `${minRT.padStart(4, " ")} ${c.yellow("/")} ${avgRT.padStart(4, " ")} ${c.yellow("/")} ${maxRT.padStart(4, " ")} ms`;
+      const adrStr = `${minADR.padStart(5, " ")} ${c.yellow("/")} ${avgADR.padStart(5, " ")} ${c.yellow("/")} ${maxADR.padStart(5, " ")}`;
+
+      const error = this.cache.get(`err:${player.meta.steam64Id}`)?.error || false;
+      if (error) {
+        if (!this.tableErrorRefresh) {
+          this.tableErrorRefresh = true;
+          setTimeout(() => {
+            this.table();
+            this.tableErrorRefresh = false;
+          }, 30000);
         }
-      });
-      const minKills = Math.min(...kills);
-      const avgKills = kills.reduce((a, b) => a + b, 0) / kills.length;
-      const maxKills = Math.max(...kills);
+      }
 
-      const minRT = Math.min(...reactionTime);
-      const avgRT = reactionTime.reduce((a, b) => a + b, 0) / reactionTime.length;
-      const maxRT = Math.max(...reactionTime);
-
-      const minADR = Math.min(...adr);
-      const avgADR = adr.reduce((a, b) => a + b, 0) / adr.length;
-      const maxADR = Math.max(...adr);
-
-      const minKDR = Math.min(...kdr);
-      const avgKDR = kdr.reduce((a, b) => a + b, 0) / kdr.length;
-      const maxKDR = Math.max(...kdr);
-
-      const minHSP = Math.min(...hsp);
-      const avgHSP = hsp.reduce((a, b) => a + b, 0) / hsp.length;
-      const maxHSP = Math.max(...hsp);
-
-      const name = player.meta.name.length === 0 ? player.meta.steam64Id : player.meta.name;
-
-      data.push([
+      this.tableData[i + 1] = [
+        error ? "❌" : "✅",
+        getTeamIcon(name),
         name,
         leetify,
         premier,
@@ -150,27 +275,48 @@ class CS2_Players_Data {
         pos,
         util,
         winRatio,
-        player.games.length,
+        player.games?.length || null,
         player.meta.faceitNickname || "❌",
-        `${minKills.toFixed(0)} / ${avgKills.toFixed(1)} / ${maxKills.toFixed(0)}`,
-        `${minKDR.toFixed(2)} / ${avgKDR.toFixed(2)} / ${maxKDR.toFixed(2)}`,
-        `${minHSP.toFixed(0)} / ${avgHSP.toFixed(0)} / ${maxHSP.toFixed(0)}`,
-        `${(minRT * 1000).toFixed(0)} / ${(avgRT * 1000).toFixed(0)} / ${(maxRT * 1000).toFixed(0)} ms`,
-        `${minADR.toFixed(0)} / ${avgADR.toFixed(0)} / ${maxADR.toFixed(0)}`,
-      ]);
+        killsStr,
+        kdrStr,
+        hspStr,
+        rtStr,
+        adrStr,
+      ];
+
+      if (this.debug) {
+        console.clear();
+        console.log(table(this.tableData, config));
+        console.log(this.dataError);
+      } else {
+        logUpdate(table(this.tableData, config));
+      }
     });
-    console.log(table(data, config));
+
+    if (this.debug) {
+      console.clear();
+      console.log(table(this.tableData, config));
+      console.log(this.dataError);
+    } else {
+      logUpdate(table(this.tableData, config));
+    }
+    if (this.dataError) {
+      this.dataError = false;
+      setTimeout(() => {
+        this.refreshData();
+      }, 2000);
+    }
   }
 
   refreshSteamIds() {
     const link = cwd() + "\\playerfetch\\PlayerFetch.exe";
     child.exec(link, (error, stdout, stderr) => {
       if (error) {
-        console.log(`error: ${error.message}`);
+        if (this.debug) console.error(`error: ${error.message}`);
         return;
       }
       if (stderr) {
-        console.log(`stderr: ${stderr}`);
+        if (this.debug) console.error(`stderr: ${stderr}`);
         return;
       }
       if (stdout) {
@@ -181,12 +327,19 @@ class CS2_Players_Data {
           .map((x) => JSON.parse(x))
           .sort((a, b) => a.steamId - b.steamId);
 
-        if (JSON.stringify(this.steamData) !== JSON.stringify(result)) {
-          console.log("Refreshing Steam Ids...");
-          this.steamData = result;
-          this.refreshData();
+        /**
+         *  3 players on wingman, 9 players on premier
+         *  TODO: Test it and find better way to handle?
+         */
+        const isFullData = result.length > 2;
+        // if (JSON.stringify(this.steamData) !== JSON.stringify(result) && isFullData) {
+        const { mySteam64Id } = this.cfg;
+        if (!isNaN(Number(mySteam64Id))) {
+          result.push({ steam64Id: mySteam64Id, timestamp: (Date.now() / 1000).toFixed(0) });
         }
-        return;
+        this.steamData = result;
+        this.refreshData();
+        // }
       }
     });
   }
@@ -195,27 +348,33 @@ class CS2_Players_Data {
     this.leetifyData = [];
 
     await Promise.all(
-      this.steamData.map(async (player) => {
+      this.steamData.map(async (player, i) => {
         const leetifyData = await this.getLeetifyProfile(player["steam64Id"]);
         if (leetifyData) {
           try {
+            if (leetifyData.games.length > this.apiLeetify.maxMatchesLeetify) {
+              leetifyData.games = leetifyData.games.slice(0, this.apiLeetify.maxMatchesLeetify);
+            }
             const matches = await Promise.all(
               leetifyData.games.map(async (game) => {
-                const match = await this.getMatchStats(game["gameId"]);
+                const match = await this.getMatchStats(game["gameId"], player["steam64Id"]);
                 if (match && match.playerStats.length > 0) {
                   return match;
                 }
               })
             );
-            leetifyData.games_full = matches;
-            this.leetifyData.push(leetifyData);
+            if (this.leetifyData.filter((p) => p.meta.steam64Id === player["steam64Id"]).length === 0) {
+              leetifyData.games_full = matches;
+              this.leetifyData.push(leetifyData);
+            }
           } catch (error) {
             this.leetifyData.push({
               meta: {
                 steam64Id: player["steam64Id"],
               },
             });
-            console.log(`Error refreshData | ${error} ${error.cause}`);
+            this.cache.set(`err:${player["steam64Id"]}`, { error: true }, 30);
+            if (this.debug) console.error(`Error refreshData | ${error} ${error.cause}`);
           }
         } else {
           this.leetifyData.push({
@@ -226,32 +385,48 @@ class CS2_Players_Data {
         }
       })
     );
-
     this.table();
   }
 
   async getLeetifyProfile(steamId) {
     try {
-      const data = await fetch(this.apiLeetify.profile + steamId).then((res) => res.json());
-      return data;
+      const isCached = this.cache.get(steamId);
+      if (isCached) {
+        return isCached;
+      } else {
+        const data = await fetch(this.apiLeetify.profile + steamId).then((res) => res.json());
+        this.cache.set(steamId, data);
+        return data;
+      }
     } catch (error) {
-      console.log(`Error fetch profile: ${steamId} | ${error} ${error.cause}`);
+      if (this.debug) console.error(`Error fetch profile: ${steamId} | ${error} ${error.cause}`);
+      this.dataError = true;
+      this.cache.set(`err:${steamId}`, { error: true }, 30);
       return false;
     }
   }
 
-  async getMatchStats(matchId) {
+  async getMatchStats(matchId, steam64Id) {
     try {
-      const data = await fetch(this.apiLeetify.games + matchId).then((res) => res.json());
-      return data;
+      const isCached = this.cache.get(matchId);
+      if (isCached) {
+        return isCached;
+      } else {
+        const data = await fetch(this.apiLeetify.games + matchId).then((res) => res.json());
+        this.cache.set(matchId, data);
+        return data;
+      }
     } catch (error) {
-      console.log(`Error fetch match: ${matchId} | ${error} ${error.cause}`);
+      if (this.debug) console.error(`Error fetch match: ${matchId} | ${error} ${error.cause}`);
+      this.dataError = true;
+      this.cache.set(`err:${steam64Id}`, { error: true }, 30);
       return false;
     }
   }
 
   startLoop() {
     this.refreshSteamIds();
+    this.table();
     setInterval(() => {
       this.refreshSteamIds();
     }, this.refreshInterval);
